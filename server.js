e40085054a9a5c5050a8c23c6f330c396efa47c3
hugh halford-thompson <hughht5@gmail.com>
2014-02-23 01:13:34 +0000

    testnet

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
 
 
   if(err) throw err; 
-  collection = db.collection('uploadedFiles4');
+  collection = db.collection('uploadedFiles');
 
   logly.log('Connection to mongo complete');
 
